28d4eda58924de81e0649034160399e801ec04de
Eric Fischer <enf@pobox.com>
2016-06-22 10:50:08 -0700

    Fix #include order

--- src/gfxline.c
+++ src/gfxline.c
@@ -26,8 +26,8 @@ CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 POSSIBILITY OF SUCH DAMAGE. */
 
-#include "gfxline.h"
 #include "gfxpoly.h"
+#include "gfxline.h"
 #include <stdlib.h>
 
 gfxline_t* gfxline_new() {
